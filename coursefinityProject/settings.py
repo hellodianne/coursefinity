caeea5009690ad421474d34bac7b79c2ec3c2ec1
hellodianne <dbronola@gmail.com>
2014-06-22 17:45:46 -0700

    change settings

--- coursefinityProject/settings.py
+++ coursefinityProject/settings.py
@@ -65,11 +65,11 @@ WSGI_APPLICATION = 'coursefinityProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'coursefinity',
-        'USER': 'dbronola',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '',
+        #'NAME': 'coursefinity',
+        #'USER': 'dbronola',
+        #'PASSWORD': '',
+        #'HOST': 'localhost',
+        #'PORT': '',
 
     }
 }
